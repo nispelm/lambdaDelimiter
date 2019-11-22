4aedb62f50bb88922019265a4c2bc742225604bd
nispelm <47509426+nispelm@users.noreply.github.com>
2019-11-22 09:57:26 -0800

    Add files via upload

---
+++ doubledelim/testdoubledelim.py
@@ -0,0 +1,19 @@
+import csv
+import sys
+import os
+
+# Convert comma-delimited CSV files to pipe-delimited files
+# Usage: Drag-and-drop CSV file over script to convert it.
+
+inputPath = "/home/matt/environment/testdelim/testdoubledelim"
+outputPath = "/home/matt/environment/testdelim/testdoubledelimout"
+
+# https://stackoverflow.com/a/27553098/3357935
+print("Converting CSV to tab-delimited file...")
+with open(inputPath) as inputFile:
+	with open(outputPath, 'w', newline='') as outputFile:
+		reader = csv.DictReader((line.replace('||', '|') for line in inputFile), delimiter='|')
+		writer = csv.DictWriter(outputFile, reader.fieldnames, delimiter='|')
+		writer.writeheader()
+		writer.writerows(reader)
+print("Conversion complete.")
